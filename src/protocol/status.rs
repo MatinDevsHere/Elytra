6521575824c68d81ebdf8457d1f47ab29db74462
Matin Mohammadi <matinmn87@gmail.com>
2025-02-06 16:14:58 +0330

    Add status packet

---
+++ src/protocol/status.rs
@@ -0,0 +1,23 @@
+﻿use crate::protocol::packet::{MinecraftPacketBuffer, Packet};
+
+pub struct StatusRequestPacket;
+
+impl Packet for StatusRequestPacket {
+    fn read(_buffer: &mut MinecraftPacketBuffer) -> std::io::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(StatusRequestPacket)
+    }
+}
+
+pub struct StatusResponsePacket {
+    pub response: String,
+}
+
+impl Packet for StatusResponsePacket {
+    fn write(&self, buffer: &mut MinecraftPacketBuffer) -> std::io::Result<()> {
+        buffer.write_string(&self.response);
+        Ok(())
+    }
+}
