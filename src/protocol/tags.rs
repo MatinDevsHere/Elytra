84fe5d154ed2a4d37bbbd6149722cde04eaae958
Matin Mohammadi <matinmn87@gmail.com>
2025-02-10 22:49:29 +0330

    Implement tags packet

---
+++ src/protocol/tags.rs
@@ -0,0 +1,36 @@
+﻿use crate::protocol::packet::{MinecraftPacketBuffer, Packet};
+
+pub struct TagsPacket {}
+
+impl Packet for TagsPacket {
+    fn packet_id() -> i32
+    where
+        Self: Sized,
+    {
+        0x5B
+    }
+
+    fn write_to_buffer(&self, buffer: &mut MinecraftPacketBuffer) -> std::io::Result<()> {
+        buffer.write_varint(Self::packet_id());
+
+        // Length of Block Tags Array
+        buffer.write_varint(0);
+
+        // Length of Item Tags Array
+        buffer.write_varint(0);
+
+        // Length of Fluid Tags Array
+        buffer.write_varint(0);
+
+        // Length of Entity Tags Array
+        buffer.write_varint(0);
+
+        Ok(())
+    }
+}
+
+impl TagsPacket {
+    pub fn new() -> TagsPacket {
+        TagsPacket {}
+    }
+}
